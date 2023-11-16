a5407ab848897bb18e20cd9c682c59d2aead75ad
raffirm <mursalimrafi008@gmail.com>
2023-11-16 00:21:49 +0000

    🟡 auto commit

--- Update.tsx
+++ Update.tsx
@@ -9567,3 +9567,7 @@ curl: (22) The requested URL returned error: 408
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
 curl: (22) The requested URL returned error: 408
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0   1008      0 --:--:-- --:--:-- --:--:--  1016
+{"code":"InvalidParameters","error":"not found","data":null}
